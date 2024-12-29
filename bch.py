b062764da1c13e06fc81411f7c829d3102dd46d3
PixelSergey <sergey.ichtchenko@gmail.com>
2024-12-29 15:33:21 +0000

    Do a lot of stuff

--- bch.py
+++ bch.py
@@ -1,13 +1,16 @@
-from sympy import GF, ZZ, Matrix, poly, Poly, div
+from sympy import GF, ZZ, Matrix, poly, Poly, div, Symbol, Add, Mul, Pow, Integer
 from sympy.abc import x
 from sympy.polys.galoistools import gf_gcdex
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 
 m = 3
 n = 2**m - 1
 
 t = 2
 generator = poly(x**8 + x**7 + x**6 + x**4 + 1, domain=GF(2))
-alpha = poly(x**4 + x**3 + x**2 + x + 1, domain=GF(2))
+alpha = poly(x**4 + x + 1, domain=GF(2))
 
 
 def polynomify(integer):
@@ -22,8 +25,7 @@ def substitute(polynomial, substitution):
     for i, coeff in enumerate(polynomial.all_coeffs()[::-1]):
         if coeff == 0:
             continue
-        result += substitution**i
-    result %= generator
+        result += Poly(substitution**i, x, domain=GF(2))
     return result
 
 
@@ -33,11 +35,44 @@ def find_inverse(polynomial):
     return Poly(inv, x, domain=GF(2))
 
 
-def expand_division(fraction):
-    numerator = Poly(fraction.args[1], x, domain=GF(2))
-    denominator = Poly(1/fraction.args[0], x, domain=GF(2))
-    inv_denominator = find_inverse(denominator)
-    return (numerator * inv_denominator) % alpha
+def find_all_powers(polynomial):
+    result = dict()
+    for i in range(1, 16):
+        power = Poly(x**i, x, domain=GF(2)) % polynomial
+        if tuple(power.all_coeffs()) in result:
+            continue
+        result[tuple(power.all_coeffs())] = i
+    return result
+
+
+def expand_expression(expression):
+    if isinstance(expression, Symbol):
+        return Poly(expression, x, domain=GF(2))
+
+    if isinstance(expression, Integer):
+        return int(expression)
+
+    if isinstance(expression, Mul):
+        result = Poly(1, x, domain=GF(2))
+        for term in expression.args:
+            result *= expand_expression(term)
+        return result
+    
+    if isinstance(expression, Add):
+        result = Poly(0, x, domain=GF(2))
+        for term in expression.args:
+            result += expand_expression(term)
+        return result
+    
+    if isinstance(expression, Pow):
+        base, exponent = expression.args
+        base = expand_expression(base)
+        exponent = int(exponent)
+        if exponent < 0:
+            base = find_inverse(base)
+            exponent = abs(exponent)
+
+        return Poly(base**exponent, x, domain=GF(2))
 
 
 def find_all_roots(polynomial, mod=generator):
@@ -59,17 +94,18 @@ def encode_bch(bits):
 
 def find_error_locator(syndromes):
     for i in range(t):
-        syndrome_matrix = Matrix(t-i, t-i, lambda x,y: syndromes[x+y])
-        detection = syndrome_matrix.det() % generator
+        nu = t-i  # Number of errors
+        syndrome_matrix = Matrix(nu, nu, lambda x,y: syndromes[x+y])
+        detection = syndrome_matrix.det() % alpha
         if detection == 0:
             continue
 
-        syndrome_vector = Matrix(t-i, 1, lambda x,_: -syndromes[t+1+x])
+        syndrome_vector = Matrix(nu, 1, lambda x,_: -syndromes[nu+x])
         augmented = syndrome_matrix.col_insert(i, syndrome_vector)
-        locator = augmented.rref()[0].col(t-i)
+        locator = augmented.rref(pivots=False).col(nu)
         result = []
-        for row in range(t-i):
-            result.append(expand_division(locator[row]))
+        for row in range(nu):
+            result.append(expand_expression(locator[row]))
 
         return result
 
@@ -77,34 +113,29 @@ def find_error_locator(syndromes):
 def find_error_pos(locator):
     print(locator)
     locator_poly = Poly(1, x, domain=GF(2))
-    for i, lam in enumerate(locator[::-1]):
-        locator_poly += Poly(lam**(i+1), x, domain=GF(2))
-
-    print(locator_poly)
-    roots = find_all_roots(locator_poly, alpha)
-    print(roots)
+    for i, lambda_i in enumerate(locator[::-1], start=1):
+        locator_poly += lambda_i * Poly(x**i, x, domain=GF(2))
     
-    pos = []
-    for i in range(1,16):
-        ai = Poly(alpha**i, x, domain=GF(2))
-        if substitute(locator_poly, ai) == 0:
-            pos.append(i)
-    print(pos)
-    print([integerify(find_inverse(polynomify(a)%alpha)%alpha) for a in pos])
-    return pos
+    powers = []
+    for power in alphapowers:
+        root = substitute(locator_poly, Poly(power, x, domain=GF(2))) % alpha
+        if root == 0:
+            inverse = find_inverse(Poly(power, x, domain=GF(2)))
+            powers.append(alphapowers[tuple((inverse%alpha).all_coeffs())])
+
+    print(powers)
+    return powers
 
 
 def decode_bch(bits):
     encoded = Poly(bits, x, domain=GF(2))
 
     syndromes = []
-    for root in generator_roots:
-        syndrome = substitute(encoded, root) % generator
-        if syndrome == 0:
-            continue
+    for i in range(1,2*t+1):
+        syndrome = substitute(encoded, x**i) % alpha
         syndromes.append(syndrome)
 
-    if not syndromes:
+    if all((syndrome == 0 for syndrome in syndromes)):
         decoded, _ = div(encoded, generator)
         return decoded.all_coeffs()
     
@@ -113,9 +144,29 @@ def decode_bch(bits):
 
 
 if __name__ == "__main__":
-    generator_roots = find_all_roots(generator)
+    alphapowers = find_all_powers(alpha)
+    
+    # print(alphapowers)
+    # for power in alphapowers:
+    #     print(alphapowers[power], Poly(power, x, domain=GF(2)), substitute(generator, Poly(power, x, domain=GF(2)))%alpha)
 
     #print(encode_bch([1,0,1,1,1,0]))
     #print(decode_bch([0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0]))
     print(decode_bch([0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0]))
     #                 ^15th pos      ^--^ errors (9 & 10)          ^ 0th position
+    print(decode_bch([0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0]))
+    #                          ^ 12           ^ 7
+
+
+    # correct = [0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0]
+    # # Test all 1-bit errors
+    # for i in range(len(correct)-1):
+    #         incorrect = correct[:i]+[1-correct[i]]+correct[i+1:]
+    #         decode_bch(incorrect)
+    # print("---")
+    # # Test all 2-bit errors
+    # for i in range(len(correct)-1):
+    #     for j in range(i+1,len(correct)):
+    #         incorrect = correct[:i]+[1-correct[i]]+correct[i+1:j]+[1-correct[j]]+correct[j+1:]
+    #         decode_bch(incorrect)
+    
